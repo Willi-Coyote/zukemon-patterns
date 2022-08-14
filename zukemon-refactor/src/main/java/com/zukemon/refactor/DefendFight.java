0ddbf2f8d9fcf12f3328457d1460cd9f935b9acc
willi.coyote@gmx.de <GitEclipse6>
2022-08-14 15:41:12 +0800

    Refactored III

--- zukemon-refactor/src/main/java/com/zukemon/refactor/DefendFight.java
+++ zukemon-refactor/src/main/java/com/zukemon/refactor/DefendFight.java
@@ -2,12 +2,6 @@ package com.zukemon.refactor;
 
 import com.zukemon.refactor.zukemons.Zukemon;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-
 public class DefendFight extends FightStyle {
 
     public DefendFight(Fight fight) {
@@ -19,38 +13,22 @@ public class DefendFight extends FightStyle {
         Zukemon attacker = fight.getZukemonFactory().createRandomZukemon();
         Zukemon defender = fight.getZukemonFactory().createRandomZukemon();
         int initialLifePoints = defender.getLifePoints();
-        // The defender gets super much life points
         defender.increaseLifePointsBy(5000);
 
         int numberOfSurvivedRounds = 0;
         while (true) {
-            int attackerDamage = attacker.hit();
-            defender.reduceLifePointsBy(attackerDamage);
-            this.fight.getArenaDisplay().update(attacker, attackerDamage);
-            if (attackerDamage > this.fight.getHighScore()) {
-                this.fight.setHighScore(attackerDamage);
-                System.out.println("New highscore from " + attacker.getClass().getSimpleName() + ": " + this.fight.getHighScore());
-            }
-            String historyRecord = "Zukemon '" + attacker.getClass().getSimpleName() + "' made " + attackerDamage + " damage at '" + defender.getClass().getSimpleName() + "'\r\n";
-            try {
-                File historyFile = new File("history.txt");
-                if (!historyFile.exists()) {
-                    historyFile.createNewFile();
-                }
-                Files.write(Paths.get("history.txt"), historyRecord.getBytes(), StandardOpenOption.APPEND);
-                if (defender.isDead()) {
-                    String deadMessage = "Zukemon '" + defender.getClass().getSimpleName() + "' has survived " + numberOfSurvivedRounds + " rounds.\r\n";
-                    Files.write(Paths.get("history.txt"), deadMessage.getBytes(), StandardOpenOption.APPEND);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            attack(attacker, defender);
             if (defender.isDead()) {
+                addHistoryEntry("Zukemon '" + defender.getClass().getSimpleName() + "' has survived " + numberOfSurvivedRounds + " rounds.\r\n");
                 return attacker;
             }
-            //heal 10% of initial lifepoints
-            defender.increaseLifePointsBy(initialLifePoints / 100 * 10);
+
+            heal10Percent(defender, initialLifePoints / 100 * 10);
             numberOfSurvivedRounds++;
         }
+    }
+
+    private void heal10Percent(Zukemon defender, int lifePointsToHeal) {
+        defender.increaseLifePointsBy(lifePointsToHeal);
     }
 }
